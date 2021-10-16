61fc4e2cd132a18d60ab8d3e6c4d00be2956b57a
Andrew Markham <andrewmarkham3@gmail.com>
2021-10-16 13:52:15 +0100

    Added JSON serialization of best boards so that progress saved. Made some properties private

--- calculate/__init__.py
+++ calculate/__init__.py
@@ -1,6 +1,20 @@
 from random import sample
 import time
 import re
+from os.path import exists
+import json
+
+
+memoization_dict = {tuple(['ssss']): 16,
+                            tuple(['sss']): 10,
+                            tuple(['ss']): 5,
+                            tuple(['s']): 2,
+                            tuple(['s.s']): 4,
+                            tuple(['s..s']): 4,
+                            tuple(['ss.s']): 7,
+                            tuple(['s.ss']): 7,
+                            tuple([]): 0
+                        }
 
 
 def random_board():
@@ -15,7 +29,7 @@ def random_board():
     all_tiles = (['s'] * 16) + (['f'] * 16) + (['1', '2', '3', '4'] * 7) + (['o'] * 16) + (['p'] * 16) + (
             ['h'] * 16)
     random_sample = sample(all_tiles, 16)
-    return [''.join(random_sample[i:i + 4]) for i in range(0, 16, 4)]
+    return [''.join(random_sample[i:i + 4]) for i in range(0, 16, 4)]  # stacks the board
 
 
 def checkNonNegIndex(num):
@@ -42,6 +56,7 @@ def evaluate_node(sub_board, run_length):
     # Example A: ['sss.', '.sss', '.s..', 'sss.'] scores 29 for 4,2,1,1,1,1 or 31 for 3,3,3,1
     # Example B: ['.sss', '..s.', '..s.', '.sss'] scores 24 for 4,1,1,1,1 or 25 for 3,3,2
     """Passed list of strings. NB: parent function must have trimmed the input"""
+
     if (tuple_board := tuple(sub_board)) in memoization_dict:  # if this sub_board calculation cached
         return memoization_dict[tuple_board]
     transp_board = transpose(sub_board)
@@ -52,8 +67,8 @@ def evaluate_node(sub_board, run_length):
     while (searches := [substr_in_list(sub_board, search_str := 's' * run_length),
                         substr_in_list(transp_board, search_str)]) == [False, False]:
         run_length -= 1
-        if run_length == 0:  # I suspect this code will never run given the whitespace trimming code to be written below
-            raise Exception("Auto-exiting from infinite loop")  # Exits from the infinite loop
+        if run_length == 0:  # Should never run as a blank grid will be trimmed to the empty list and scored as zero.
+            raise Exception("Auto-exiting from infinite loop")
 
     # There is at least one run of length == run_length
     # check each possible removal, and call recursion on each one after trimming whitespace
@@ -64,7 +79,7 @@ def evaluate_node(sub_board, run_length):
     for dimension in range(n, 2):
         for i in range(len(sub_board)):
             if sub_board[i].count(
-                    search_string := ('s' * run_length)) > 0:  # I don't know why it would be greater than 1
+                    search_string := ('s' * run_length)) > 0:
                 temp_board = sub_board.copy()  # create temporary copy of board for testing
                 temp_board[i] = temp_board[i].replace(search_string, '.' * run_length)
                 branch_scores.append(memoization_dict[tuple([search_string])] + evaluate_node(trim_board(temp_board),
@@ -74,28 +89,36 @@ def evaluate_node(sub_board, run_length):
     return max_node
 
 
-def trim_board(sub_board):
+def trim_board(grid):
     trim_matches = ['....', '...', '..', '.']
-    try:
-        while sub_board[0] in trim_matches:
-            sub_board = sub_board[1:]  # trim top
-        while sub_board[-1] in trim_matches:
-            sub_board = sub_board[:-1]
-        transp_board = transpose(sub_board)  # transposed once
-        while transp_board[0] in trim_matches:
-            transp_board = transp_board[1:]  # trim top
-        while transp_board[-1] in trim_matches:
-            transp_board = transp_board[:-1]
-        return transpose(transp_board)  # transposed twice
-    except IndexError:
-        return []
+    for dimension in range(2):  # trim, transpose, trim, transpose
+        if not grid:  # if empty list passed
+            return []
+        while grid and grid[0] in trim_matches:
+            grid = grid[1:]  # trim top
+        if not grid:  # if empty list passed
+            return []
+        while grid[-1] in trim_matches:
+            grid = grid[:-1]
+        if len(grid) > 2:  # is there a middle row to consider?
+            i = 1  # set index to middle row
+            while i < len(grid) - 1:  # while considering middle rows
+                # check whether it is safe to delete this row (does it preserve runs correctly)
+                safe_to_delete = not any([grid[i - 1][j] == 's' and grid[i + 1][j] == 's' for j in range(len(grid[0]))])
+                if grid[i] in trim_matches and safe_to_delete:
+                    # safe to delete unneeded middle row. i not incremented as row deletion instead
+                    grid = grid[:i] + grid[i + 1:]
+                else:
+                    # not safe to delete middle row. i incremented
+                    i += 1
+        grid = transpose(grid)
+    return grid  # transposed twice
 
 
 def transpose(str_list):
     """Transposes (exchanges columns and rows) of the board layout."""
     # takes list of strings of dimensions n x m
-    n = len(str_list)
-    if n == 0:
+    if (n := len(str_list)) == 0:
         return []
     if (m := len(str_list[0])) == 1 and n == m:
         return str_list
@@ -107,22 +130,29 @@ def transpose(str_list):
     return transposed_board
 
 
-memoization_dict = {tuple(['ssss']): 16,
-                    tuple(['sss']): 10,
-                    tuple(['ss']): 5,
-                    tuple(['s']): 2,
-                    tuple(['s.s']): 4,
-                    tuple(['s..s']): 4,
-                    tuple(['ss.s']): 7,
-                    tuple(['s.ss']): 7,
-                    tuple([]): 0
-                    }
-
-
 class Board:
-    def __init__(self):
-        self.as_list = random_board()
-        self.as_str = ''.join(self.as_list)
+    def __init__(self, board):
+        self._as_list = board
+        self.__as_str = ''.join(self.as_list)
+
+    def to_dict(self):
+        dump = {
+            '__board__': True,
+            'board': self.as_list,
+            'points': self.points,
+            'sub_totals':
+                {'entertainment': self.entertainment,
+                 'factories': self.factories,
+                 'houses': self.houses,
+                 'offices': self.offices,
+                 'parks': self.parks,
+                 'shopping': self.shopping}
+        }
+        return dump
+
+    @property
+    def as_list(self):
+        return self._as_list
 
     @property
     def entertainment(self):
@@ -135,13 +165,13 @@ class Board:
                     4       17
             """
         num = 0
-        if "1" in self.as_str:
+        if "1" in self.__as_str:
             num += 1
-        if "2" in self.as_str:
+        if "2" in self.__as_str:
             num += 1
-        if "3" in self.as_str:
+        if "3" in self.__as_str:
             num += 1
-        if "4" in self.as_str:
+        if "4" in self.__as_str:
             num += 1
         point_dict = {0: 0, 1: 1, 2: 4, 3: 9, 4: 17}
         return point_dict[num]
@@ -153,7 +183,7 @@ class Board:
             Second and third place score 3 and 2 points per factory respectively.
             Since this code only attempts to determine the maximum score of a given board, the maximum multiplier is used.
             """
-        return self.as_str.count("f") * 4
+        return self.__as_str.count("f") * 4
 
     @property
     def houses(self):
@@ -163,15 +193,15 @@ class Board:
         num = 0
         points = 0
         # TODO: add pattern matching
-        if "s" in self.as_str:
+        if "s" in self.__as_str:
             num += 1
-        if "f" in self.as_str:
+        if "f" in self.__as_str:
             num += 1
-        if "1" in self.as_str or "2" in self.as_str or "3" in self.as_str or "4" in self.as_str:
+        if "1" in self.__as_str or "2" in self.__as_str or "3" in self.__as_str or "4" in self.__as_str:
             num += 1
-        if "o" in self.as_str:
+        if "o" in self.__as_str:
             num += 1
-        if "p" in self.as_str:
+        if "p" in self.__as_str:
             num += 1
         for i in range(4):
             for j in range(4):
@@ -208,11 +238,10 @@ class Board:
                     6+       21
             There is an additional point scored for each office that shares an edge with an entertainment venue."""
         point_dict = [0, 1, 3, 6, 10, 15, 21]
-        points = 0
-        if (num_offices := self.as_str.count('o')) > 6:
-            points += 21
+        if (num_offices := self.__as_str.count('o')) > 6:
+            points = 21
         else:
-            points += point_dict[num_offices]
+            points = point_dict[num_offices]
         for i in range(4):
             for j in range(4):
                 if self.as_list[i][j] == 'o':
@@ -285,22 +314,37 @@ class Board:
         return '\n'.join([' '.join(char) for char in self.as_list])
 
 
+def as_board(dct):
+    if '__board__' in dct:
+        return Board(dct['board'])
+    return dct
+
+
 if __name__ == '__main__':
+    if exists('best_boards.json'):
+        with open('best_boards.json', 'r') as read_file:
+            best_boards = json.load(read_file, object_hook=as_board)
+            best_score = best_boards[0].points
+    else:
+        best_boards = []
+        best_score = 0
+    x = 10000000 # 20 minutes
+    length = len(memoization_dict)
     tic = time.perf_counter()
-    best_boards = []
-    best_score = 0
-    x = 5000000
-    length = 0
+    toc = tic
     try:
         for i in range(x + 1):
             if i >= (n := 10000) and i % n == 0:
-                print(f'{i} solutions tested in {0 - tic + (tic := time.perf_counter())} seconds')
-                print(f'    {0 - length + (length := len(memoization_dict))} entries added to dict')
-            if (new_score := (b := Board()).points) >= best_score:
+                print(f'{i} solutions tested in another {(0 - toc + (toc := time.perf_counter())):.4f} seconds with {0 - length + (length := len(memoization_dict))} entries added to dict')
+            if (new_score := (new_board := Board(random_board())).points) >= best_score:
                 if new_score > best_score:
                     best_boards = []
                     best_score = new_score
-                best_boards.append(b)
+                    print(f'New best score: {best_score}')
+                else:
+                    print(f'Another board found with best score: {best_score}')
+                best_boards.append(new_board)
+
     except KeyboardInterrupt:
         i -= 1
 
@@ -309,3 +353,8 @@ if __name__ == '__main__':
     print(f'Optimal score: {best_score}')
     print(f'Number of solutions for this score: {len(best_boards)}')
     print(f'Best board:\n{best_boards[0]}')
+    print(f'{len(memoization_dict)} entries in dict')
+    print(memoization_dict)
+
+    with open('best_boards.json', 'w') as f:
+        f.write(json.dumps([b.to_dict() for b in best_boards], sort_keys=True, indent=4))
